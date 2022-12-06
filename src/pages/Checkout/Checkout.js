cf8d8f6c5222ad0e5168c27ec7b4bde3d401ac0f
NazPro <najmulbinnurul3@gmail.com>
2022-12-06 21:22:13 +0600

    readme edited

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import { useLoaderData } from 'react-router-dom';
 import {BiCheckDouble} from "react-icons/bi";
+import Footer from '../Shared/Footer/Footer';
 
 const Checkout = () => {
     const [purchase, setPurchase]=useState(false)
@@ -9,18 +10,21 @@ const Checkout = () => {
     }
     const checkoutDetails=useLoaderData()
     return (
-        <div className='container mx-auto lg:m-20'>
-            <div className="card w-96 bg-base-100 shadow-xl mx-auto border">
-                <figure><img src={checkoutDetails.img} alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{checkoutDetails.courseName}</h2>
-                    <p>You are about to purchase the course</p>
-                    <div className="card-actions justify-end">
-                        <button disabled={purchase? true:false} onClick={handleClick} className="btn text-primary hover:bg-gray-200 bg-transparent border-0">
-                            {purchase ?<><BiCheckDouble className='text-xl'/>Added</> :'continue'}</button>
+        <div>
+            <div className='container mx-auto lg:m-20'>
+                <div className="card w-96 bg-base-100 shadow-xl mx-auto border">
+                    <figure><img src={checkoutDetails.img} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{checkoutDetails.courseName}</h2>
+                        <p>You are about to purchase the course</p>
+                        <div className="card-actions justify-end">
+                            <button disabled={purchase? true:false} onClick={handleClick} className="btn text-primary hover:bg-gray-200 bg-transparent border-0">
+                                {purchase ?<><BiCheckDouble className='text-xl'/>Added</> :'continue'}</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -5,43 +5,47 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Autoplay, Pagination} from "swiper";
+import Footer from '../Shared/Footer/Footer'
 
 
 const Courses = () => {
     const courseCategories=useLoaderData()
 
     return (
-        <div className="grid grid-cols-8 gap-8 container mx-auto ">
-            <div className='col-span-2'>
-                <div className="card bg-base-100 shadow-xl ">
-                    <div className="card-body">
-                        <>
-                            <h2 className="card-title mb-3">Our Top Courses</h2>
-                            {
-                                courseCategories.map(course=><Course key={course.id} course={course}></Course>)
-                            }
-                        </>
-                        <Swiper
-                            className='w-full mt-8 hover:'
-                            spaceBetween={50}
-                            loop={true}
-                            autoplay={{delay:2500,pauseOnMouseEnter:true, disableOnInteraction:false}}
-                            slidesPerView={1}
-                            pagination={{type:'progressbar',}}
-                            modules={[Autoplay,Pagination]}
-                            >
-                            {
-                                courseCategories.map(course=><SwiperSlide key={course.id2}>
-                                    <img className='w-full ' src={course.icon} alt="" />
-                                </SwiperSlide>)
-                            }
-                        </Swiper>    
+        <div>
+            <div className="grid grid-cols-8 gap-8 container mx-auto ">
+                <div className='col-span-2'>
+                    <div className="card bg-base-100 shadow-xl ">
+                        <div className="card-body">
+                            <>
+                                <h2 className="card-title mb-3">Our Top Courses</h2>
+                                {
+                                    courseCategories.map(course=><Course key={course.id} course={course}></Course>)
+                                }
+                            </>
+                            <Swiper
+                                className='w-full mt-8 hover:'
+                                spaceBetween={50}
+                                loop={true}
+                                autoplay={{delay:2500,pauseOnMouseEnter:true, disableOnInteraction:false}}
+                                slidesPerView={1}
+                                pagination={{type:'progressbar',}}
+                                modules={[Autoplay,Pagination]}
+                                >
+                                {
+                                    courseCategories.map(course=><SwiperSlide key={course.id2}>
+                                        <img className='w-full ' src={course.icon} alt="" />
+                                    </SwiperSlide>)
+                                }
+                            </Swiper>    
+                        </div>
                     </div>
                 </div>
+                <div className="col-span-6">
+                    <Outlet></Outlet>
+                </div>
             </div>
-            <div className="col-span-6">
-                <Outlet></Outlet>
-            </div>
+            <Footer></Footer>
         </div>
     );
 };
